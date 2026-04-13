ce151ec8c5b984254c566f6714a90d55ffcfc074
Nurbu <lamanurbu2@gmail.com>
2026-04-13 13:23:32 -0400

    Addded the questions to ask the user like first,middle,last name and their suffix.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -1,4 +1,29 @@
 package com.pluralsight;
 
+import java.util.Scanner;
+
 public class FullNameApplication {
+    public static void main(String[] args) {
+        // Puts first and last name together. Will include middle name and suffix if provided.
+
+        Scanner scanner = new Scanner(System.in);
+
+        // Asks the user for information
+
+        System.out.println("Please enter your name.");
+
+        System.out.print("First name: ");
+        String firstName = scanner.nextLine().trim();
+
+        System.out.print("Middle name: ");
+        String middleName = scanner.nextLine().trim();
+
+        System.out.print("Last name: ");
+        String lastName = scanner.nextLine().trim();
+
+        System.out.print("Suffix: ");
+        String suffix = scanner.nextLine().trim();
+
+        
+    }
 }
