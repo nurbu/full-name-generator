8e20464e8561600cc14dbe81d27112b3ba57561a
Nurbu <lamanurbu2@gmail.com>
2026-04-13 13:43:14 -0400

    Added comments for the getFullname method.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -24,6 +24,7 @@ public class FullNameApplication {
         System.out.print("Suffix: ");
         String suffix = scanner.nextLine().trim();
 
+        // Calls the getFullName and gets the full name and gets stored in fullName
         String fullName = getFullName(firstName, middleName, lastName, suffix);
         System.out.println(fullName);
 
@@ -31,10 +32,16 @@ public class FullNameApplication {
     }
 
     public static String getFullName(String firstName, String middleName, String lastName, String suffix) {
+        /*Checks if there is a middle name. If yes get the first character and add a "." to it.
+        If no middle name leave it blank
+        * */
         String middleFirstLetter = "";
         if (middleName.length() > 0) {
             middleFirstLetter = middleName.substring(0, 1).toUpperCase() + ".";
         }
+        /* Check if there is a suffix. If yes return the full name but with a "," before the suffix.
+        If no suffix you just return the rest of the name as the middle name logic is completed already.
+         */
         return (suffix.length() > 0) ? firstName + middleFirstLetter + lastName + ", " + suffix :
                 firstName + middleFirstLetter + lastName;
 
