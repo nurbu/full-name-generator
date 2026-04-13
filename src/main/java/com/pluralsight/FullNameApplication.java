f6a51417d14c767261dc9bdeb3b48341d3f113b5
Nurbu <lamanurbu2@gmail.com>
2026-04-13 13:58:27 -0400

    Made firstname and lastname mandatory with while loops. Fixed spacing issues with middleFirstLetter and updated overall comments.

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -4,6 +4,11 @@ import java.util.Scanner;
 
 public class FullNameApplication {
     public static void main(String[] args) {
+        // Variables used
+        String firstName = "";
+        String lastName = "";
+        String middleName, suffix, fullName;
+
         // Puts first and last name together. Will include middle name and suffix if provided.
 
         Scanner scanner = new Scanner(System.in);
@@ -11,38 +16,41 @@ public class FullNameApplication {
         // Asks the user for information
 
         System.out.println("Please enter your name.");
-
-        System.out.print("First name: ");
-        String firstName = scanner.nextLine().trim();
-
+        // Make sures we get a first name.
+        while (firstName.length() == 0) {
+            System.out.print("First name: ");
+            firstName = scanner.nextLine().trim();
+        }
         System.out.print("Middle name: ");
-        String middleName = scanner.nextLine().trim();
-
-        System.out.print("Last name: ");
-        String lastName = scanner.nextLine().trim();
-
+        middleName = scanner.nextLine().trim();
+        // Make sures we get a last name.
+        while (lastName.length() == 0) {
+            System.out.print("Last name: ");
+            lastName = scanner.nextLine().trim();
+        }
         System.out.print("Suffix: ");
-        String suffix = scanner.nextLine().trim();
+        suffix = scanner.nextLine().trim();
 
         // Calls the getFullName and gets the full name and gets stored in fullName
-        String fullName = getFullName(firstName, middleName, lastName, suffix);
+        fullName = getFullName(firstName, middleName, lastName, suffix);
         System.out.println(fullName);
 
+        scanner.close();
 
     }
 
     public static String getFullName(String firstName, String middleName, String lastName, String suffix) {
-        /*Checks if there is a middle name. If yes get the first character and add a "." to it.
-        If no middle name leave it blank
-        * */
-        String middleFirstLetter = "";
+        /*Checks if there is a middle name. If yes, get the first character of middle name and add a "." to it.
+        If no middle name leave it blank.
+        */
+        String middleFirstLetter = " ";
         if (middleName.length() > 0) {
-            middleFirstLetter = middleName.substring(0, 1).toUpperCase() + ".";
+            middleFirstLetter = " " + middleName.substring(0, 1).toUpperCase() + ". ";
         }
         /* Check if there is a suffix. If yes return the full name but with a "," before the suffix.
-        If no suffix you just return the rest of the name as the middle name logic is completed already.
+        If no suffix, just returns the rest of the name as the middle name logic is completed already.
          */
-        return (suffix.length() > 0) ? firstName + " " + middleFirstLetter + " " + lastName + ", " + suffix :
+        return (suffix.length() > 0) ? firstName + middleFirstLetter + lastName + ", " + suffix :
                 firstName + middleFirstLetter + lastName;
 
 
